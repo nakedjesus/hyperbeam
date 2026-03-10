9362045afd4a13ec5e1c8077f49b0a07db56d96e
nakedjesus <unclothedjesus@gmail.com>
2026-03-10 19:41:20 -0400

    Add start URL to cloud computer creation request
    Updated the cloud computer creation endpoint to include a start URL.

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 
-// API Key Check
 const apiKey = process.env.HB_API_KEY
-
 if (!apiKey || apiKey === "") {
     console.error("API Key is not set, did you set the HB_API_KEY environment variable to your API key?")
 }
@@ -14,17 +12,17 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'))
 })
 
-// Get a cloud computer object. If no object exists, create it.
 let computer
 app.get('/computer', async (req, res) => {
   if (computer) {
     res.send(computer)
     return
   }
-  const resp = await axios.post('https://engine.hyperbeam.com/v0/vm', {}, {
+  const resp = await axios.post('https://engine.hyperbeam.com/v0/vm', {
+    start_url: "https://fmhy.net"
+  }, {
     headers: { 'Authorization': `Bearer ${apiKey}` }
   })
-
   computer = resp.data
   res.send(computer)
 })
